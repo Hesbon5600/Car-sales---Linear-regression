5ffec6e8508cf35b0e0b115dede1ed5cb9de7c62
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2019-06-24 10:36:53 +0300

    Linear regression exaple
    - Car sales dataset
    - Tring to predict the price on a used car

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A real life example of car sales dataa',
+    author='Hesbon',
+    license='MIT',
+)
